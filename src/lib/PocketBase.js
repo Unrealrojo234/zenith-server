0a94f4e084e5330a04559e80c5d11370d5bb13b6
Unrealrojo234 <otienoryan812@gmail.com>
2025-07-15 22:13:48 +0300

    Added withdrawal apis

---
+++ src/lib/PocketBase.js
@@ -0,0 +1,8 @@
+import  Pocketbase from 'pocketbase';
+import { PUBLIC_PB_URL } from '$env/static/public';
+
+
+const pb = new PocketBase(PUBLIC_PB_URL);
+
+
+export default pb;
---
+++ src/routes/api/withdrawal/+server.js
@@ -0,0 +1,72 @@
+import pb from '$lib/PocketBase';
+import { json } from '@sveltejs/kit';
+
+export async function POST({ request }) {
+	const data = await request.json();
+
+	if (!data.uid || !data.amount || !data.processing || !data.disbursed || !data.declined) {
+		return new Response(
+			JSON.stringify({
+				success: false,
+				info: 'Missing required fields i.e uid, amount, processing, disbursed, declined'
+			})
+		);
+	}
+
+	try {
+		const record = await pb.collection('widthdrawal').create(data);
+
+		if (record) {
+			return new Response(
+				JSON.stringify({
+					success: true,
+					info: 'Record was successful'
+				})
+			);
+		} else {
+			return new Response(
+				JSON.stringify({
+					success: false,
+					info: 'An error occured'
+				})
+			);
+		}
+	} catch (error) {
+		return new Response(
+			JSON.stringify({
+				success: false,
+				error: error.message
+			})
+		);
+	}
+}
+
+export async function GET() {
+	try {
+		const records = await pb.collection('widthdrawal').getFullList({
+			sort: '-created'
+		});
+
+		if (records) {
+			return new Response(
+				JSON.stringify({
+					success: true,
+					data: records
+				})
+			);
+		}
+
+		return new Response(
+			JSON.stringify({
+				success: false,
+				info: 'Error fetch`ing records'
+			})
+		);
+	} catch (error) {
+        
+        return new Response(JSON.stringify({
+            success:false,
+            error:error.mess
+        }))
+    }
+}
